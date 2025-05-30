1708b47e28b93759e6d21cbab8e1c22cce0d8375
José Pérez <josepc15@gmail.com>
2025-05-29 19:51:54 -0600

    Upgrade stylelint v15.11.0

---
+++ apps/web/.stylelintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@bet-tracker/stylelint-config'],
+};
--- packages/config/stylelint-config/index.js
+++ packages/config/stylelint-config/index.js
@@ -4,26 +4,22 @@ module.exports = {
       "stylelint-config-standard-scss",
       "stylelint-config-tailwindcss",
     ],
-    plugins: ["stylelint-scss", "stylelint-order"],
+    plugins: [
+      "stylelint-scss", 
+      "stylelint-order",
+    ],
     rules: {
-      "declaration-no-important": true,
-      "indentation": [2, {
-        "baseIndentLevel": 2
-      }],
-      "order/order": [
-        "custom-properties",
-        "declarations",
-        "rules",
-        "at-rules",
-      ],
-      "no-empty-source": null,
-      "string-quotes": "double",
-      "indentation": 2,
-      "color-hex-case": "lower",
+      'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['theme'] }],
       "color-hex-length": "short",
+      "declaration-block-no-duplicate-properties": null,
+      "declaration-no-important": true,
+      "no-empty-source": null,
+      "order/order": ["custom-properties", "declarations", "rules", "at-rules"],
+      "scss/at-import-partial-extension-blacklist": ["scss"],
       "scss/dollar-variable-pattern": "^([a-z][a-z0-9]*)$",
       "scss/percent-placeholder-pattern": "^([a-z][a-z0-9]*)$",
-      "scss/at-import-partial-extension-blacklist": ["scss"],
+      "selector-anb-no-unmatchable": null,
+      "selector-anb-no-unmatchable": null,
     }
   };
   
