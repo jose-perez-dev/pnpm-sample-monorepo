a89f096290024b75cfcbb200b1adec43af9288de
José Pérez <josepc15@gmail.com>
2025-06-18 20:26:55 -0600

    Fix wrong path on jest files

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   ...baseConfig,
   roots: ['<rootDir>'],
   projects: ['<rootDir>/apps/web/jest.config.js', '<rootDir>/packages/ui/jest.config.js'],
-  setupFilesAfterEnv: ['<rootDir>/config/jest/jest.setup.ts'],
-  setupFiles: ['<rootDir>/config/jest/jest.env.js'],
+  setupFilesAfterEnv: ['<rootDir>/configs/jest/jest.setup.js'],
+  setupFiles: ['<rootDir>/configs/jest/jest.env.js'],
 };
